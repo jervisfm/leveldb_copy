7c8a31a82b27314744be8d7a5f4bcaa10bee3a5f
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-12-29 07:13:22 +0000

    Add convenient leveldb lib header

---
+++ leveldb.h
@@ -0,0 +1,22 @@
+// This is a header file for the LevelDB Public API
+// found under "include/leveldb". Including it will
+// auto-include the public API of LevelDB. 
+
+#ifndef _REPOBUILD_LEVELDB__
+#define _REPOBUILD_LEVELDB__
+
+#include "leveldb/c.h"
+#include "leveldb/cache.h"
+#include "leveldb/comparator.h"
+#include "leveldb/db.h"
+#include "leveldb/env.h"
+#include "leveldb/filter_policy.h"
+#include "leveldb/iterator.h"
+#include "leveldb/options.h"
+#include "leveldb/slice.h"
+#include "leveldb/status.h"
+#include "leveldb/table_builder.h"
+#include "leveldb/table.h"
+#include "leveldb/write_batch.h"
+
+#endif  // _REPOBUILD_LEVELDB__
